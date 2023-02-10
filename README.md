49c3b0abdaf2761706da9961973901eaa7999fac
fabio <fabio.knupp@gmail.com>
2023-02-10 14:56:43 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# abrir o cypress
+npx cypress open
+
+# rodar cli headless
+npx cypress run
+
+# gragar os testes
+npx cypress run --record --key 9d400702-0f18-40f3-9b75-fe804e27192c
---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "yyimbn",
+  e2e: {
+    setupNodeEvents(on, config) {
+      
+    },
+  },
+});
