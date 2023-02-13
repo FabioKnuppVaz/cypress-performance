50637a0545f80047b54f82410cf5078206e98d6d
fabio <fabio.knupp@gmail.com>
2023-02-13 17:43:18 -0300

    revert

--- cypress/e2e/testesFront.cy.js
+++ cypress/e2e/testesFront.cy.js
@@ -1,11 +1,9 @@
-Cypress._.times(10, () => {
-    describe('testes front 1', () => {
-        it('login via front de sucesso', () => {
-            cy.visit('https://seubarriga.wcaquino.me/login')
-            cy.get('#email').type('fabio.knupp@gmail.com')
-            cy.get('#senha').type('knupp123')
-            cy.xpath('//button').click()
-            cy.xpath("//div[contains(@class,'alert')]").should('contain', 'Bem vindo, Fábio!')
-        })
+describe('testes front 1', () => {
+    it('login via front de sucesso', () => {
+        cy.visit('https://seubarriga.wcaquino.me/login')
+        cy.get('#email').type('fabio.knupp@gmail.com')
+        cy.get('#senha').type('knupp123')
+        cy.xpath('//button').click()
+        cy.xpath("//div[contains(@class,'alert')]").should('contain', 'Bem vindo, Fábio!')
     })
 })
